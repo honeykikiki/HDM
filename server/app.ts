c815f4f78481ec004e0109f06c03a93129eb601c
honeykikiki <tjdwls75666@naver.com>
2021-12-02 22:07:33 +0900

    init

---
+++ server/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.static("build"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/build/index.html");
+});
+
+app.listen(3000, () => {
+  console.log("3000번 포트 대기중");
+});
