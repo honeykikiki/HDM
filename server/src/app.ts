120d367e42350a4df34039ef9b4d32251a0b29ff
honeykikiki <tjdwls75666@naver.com>
2021-12-02 22:27:47 +0900

    스크립트 재설정

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static("build"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/build/index.html");
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("3000번 포트 대기중");
 });
